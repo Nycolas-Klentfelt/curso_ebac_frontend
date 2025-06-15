7700040ce1325dac5a1a5a23025f3f1014d31809
Nycolas Klentfelt <nyalk03@gmail.com>
2025-06-15 15:49:49 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+;
+const resultaMultiplicacao = multiplicacao(2, 3);
+console.log(resultaMultiplicacao);
+function saudacao(nome) {
+    return `Olá ` + nome;
+}
+;
+const saudacaoNome = saudacao("Nycolas");
+console.log(saudacaoNome);
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+function multiplicacao(a: number, b:number): number{
+    return a * b ;
+};
+
+const resultaMultiplicacao : number = multiplicacao(2, 3);
+console.log(resultaMultiplicacao);
+
+function saudacao(nome : string){
+    return `Olá ` + nome;
+};
+
+const saudacaoNome : string = saudacao("Nycolas");
+console.log(saudacaoNome);
